408457f5e3ae7938d6872e25b2bed28ac4209f9d
Adolfo Rangel <adolfo.a.rangel@gmail.com>
2020-01-13 17:52:15 -0500

    Added Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@
 
 module.exports = {
   /* Your site config here */
-  plugins: [`gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-156198766-1",
+        head: true,
+        pageTransitionDelay: 0,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "adolforangel.com",
+      },
+    }
+  ],
   pathPrefix: "/",
 }
